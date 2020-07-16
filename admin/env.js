6ba740ca7a5607998a1aa89c8afa46c9f119fafa
jiaozibufu <w1796629791@gmail.com>
2020-07-16 11:28:49 +0800

    v2board

---
+++ admin/env.js
@@ -0,0 +1,14 @@
+window.settings = {
+  // 站点标题
+  title: 'Fysee',
+  // API
+  host: 'https://panel.wcnm.shop',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: ''
+}
---
+++ user/env.js
@@ -0,0 +1,16 @@
+window.settings = {
+  // 站点标题
+  title: 'Fysee',
+  // 站点描述
+  description: 'Node is best',
+  // API
+  host: 'https://panel.wcnm.shop',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: ''
+}
